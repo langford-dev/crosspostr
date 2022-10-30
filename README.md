d19f2b5f4738bef0663055cb6b6ef8ceb98522cf
langford <langfordquarshie21@gmail.com>
2022-10-30 09:58:43 +0000

    project created

---
+++ README.md
@@ -0,0 +1,10 @@
+# Crosspostr
+
+An open source tool for sharing blog posts across multiple blogging platforms.
+
+## Features
+
+- SEO optimization suggestions
+- Post previews
+- New post alert on socials
+- ...much more with one click.
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <>
+      <h1>Crosspostr</h1>
+      <p> An open source tool for sharing blog posts across multiple blogging platforms. It offers SEO optimization suggestions, post previews, social media post alerts, and much more with one click. </p>
+    </>
+  )
+}
